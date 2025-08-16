ee15a7ac19533b16dd920c54eaff451522ea68b5
ayeshaakter02 <ayeshaakter0123456789@gmail.com>
2025-08-16 12:18:27 +0600

    index

---
+++ src/pages/index.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import Swal from 'sweetalert2';
+
+import Header from './Header';
+import List from './List';
+import Add from './Add';
+import Edit from './Edit';
+
+import { employeesData } from '../../data';
+
+function Dashboard() {
+
+
+    return (
+        <div className='container'>
+            {/* List */}
+            {!isAdding && !isEditing && (
+                <>
+                    <Header
+                        setIsAdding={setIsAdding}
+                    />
+                    <List
+                        employees={employees}
+                        handleEdit={handleEdit}
+                        handleDelete={handleDelete}
+                    />
+                </>
+            )}
+            {/* Add */}
+            {isAdding && (
+                <Add
+                    employees={employees}
+                    setEmployees={setEmployees}
+                    setIsAdding={setIsAdding}
+                />
+            )}
+            {/* Edit */}
+            {isEditing && (
+                <Edit
+                    employees={employees}
+                    selectedEmployee={selectedEmployee}
+                    setEmployees={setEmployees}
+                    setIsEditing={setIsEditing}
+                />
+            )}
+        </div>
+    )
+}
+
+export default Dashboard;
